7f2dbd3d67774fb77839061f32c33dc9c5d742f1
Thisara <sasmithajayasinghe1@gmail.com>
2024-01-18 15:04:39 +0530

    com

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -17,7 +17,7 @@ app.use(express.urlencoded({extended:true}))
 
 //access cookie in frontend terminal storage icon (over cookie is stored there)
 app.use(cors({
-    origin: 'https://mern-booking-app-h5w9.onrender.com',
+    origin: process.env.FRONTEND_URL,
     credentials:true
 }))
 
