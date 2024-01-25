29acc25cca6b5da43895d304a3033f40b978ae15
ThisaraJ <124505409+ThisaraJayas@users.noreply.github.com>
2024-01-25 21:10:32 +0530

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -14,6 +14,7 @@ function App() {
 
   return (
     <Routes>
+      <meta name="google-adsense-account" content="ca-pub-7741658003653293">
       <Route path='/' element={<Layout>
         <p>Home Page</p>
       </Layout>}/>
